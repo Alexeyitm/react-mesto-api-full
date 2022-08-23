a4519bd45a7c8877c53719ad4f17d3fb01e5dec9
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-08-23 12:32:42 +0500

    fix: fixed auth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,17 +2,16 @@
 const jwt = require('jsonwebtoken');
 const LoginDataError = require('../errors/login-data-error');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
+const { JWT_SECRET = 'dev-key' } = process.env;
 
 module.exports = (req, res, next) => {
   if (!req.cookies.jwt) {
     throw new LoginDataError('Необходима авторизация!');
   }
   const token = req.cookies.jwt;
-  console.log(JWT_SECRET);
   let payload;
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     throw new LoginDataError('Необходима авторизация!');
   }
